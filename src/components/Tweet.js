6eba1f2d7512b872f14d3c437509e864fa652611
ulissestb <ulisses.melo.nascimento@gmail.com>
2018-12-09 12:47:26 -0200

    FEAT: create UI of TweetPage and NewTweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { formatTweet, formatDate } from '../utils/helpers'
+import { handleToggleTweet } from '../actions/tweets'
 
 import { TiArrowBackOutline } from 'react-icons/ti/index'
 import { TiHeartOutline } from 'react-icons/ti/index'
@@ -9,6 +10,14 @@ import { TiHeartFullOutline } from 'react-icons/ti/index'
 class Tweet extends Component {
     handleLike = (e) => {
         e.preventDefault()
+
+        const { dispatch, tweet, authedUser } = this.props
+        
+        dispatch(handleToggleTweet({
+            id: tweet.id,
+            hasLiked: tweet.hasLiked,
+            authedUser
+        }))
     }
 
     toParent = (e, id) => {
